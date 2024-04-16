b87f7c8d5b37ceb3401e4773fc0d4a1f9170c559
Lea Montoya <leamontoya19@gmail.com>
2024-04-16 14:13:41 +0200

    commit

---
+++ app.js
@@ -0,0 +1,7 @@
+//este file es básicamente el server (servidor)
+
+import express from 'express'
+
+const app = express()
+
+export default app
---
+++ index.js
@@ -0,0 +1,5 @@
+//es el encargado del arranque de la app
+import app from './app.js'
+
+app.listen (3000)
+console.log ('Server on port', 3000)
