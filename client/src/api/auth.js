8e10ec62241b92ae0cbb567690a2994459dcbc52
Lea Montoya <leamontoya19@gmail.com>
2024-04-28 09:59:20 +0200

    auth.js

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from './axios.js';
 
 
 
 
 export const registerReq = (user) => axios.post(`/register`, user);
 export const loginReq = (user) => axios.post(`/login`, user);
-export const verifyTokenReq = () => axios.get( '/verification')
+export const verifyTokenReq = () => axios.get( '/verification');
