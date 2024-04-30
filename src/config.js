15362755fd637e2deb057336772c59fc971bbb1c
Lea Montoya <leamontoya19@gmail.com>
2024-04-30 13:03:04 +0200

    config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const PORT = 4000
 export const TOKEN_SECRET = 'some secret key';
-export const FRONTEND_URL = 'http://localhost:5173';
+export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 
